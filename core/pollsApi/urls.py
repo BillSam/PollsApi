6be0278254c00175cb7bdc12d58e0ed3c4ad812e
sam mbugua <sam@systech>
2022-02-01 19:59:32 +0300

    added views and urls routers

--- core/pollsApi/urls.py
+++ core/pollsApi/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 from .views import poll_list, poll_details
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from rest_framework.routers import DefaultRouter
+from .apiviews import PollViewSet
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, basename='polls')
 
 urlpatterns = [
-    path("polls/", poll_list, name="polls_list"),
-    path("polls/<int:pk>", poll_details, name="polls_details")
+    # path("polls/", PollList.as_view(), name="polls_list"),
+    # path("polls/<int:pk>", PollDetail.as_view(), name="polls_details"),
+    # path("choices/", ChoiceList.as_view(), name="choice_list"),
+    # path("vote/", CreateVote.as_view(), name="create_vote"),
+
+    path("polls/<int:pk>/choices", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+
 ]
+
+urlpatterns += router.urls
--- core/pollsApi/views.py
+++ core/pollsApi/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .models import Poll
 
 
 def poll_list(request):
-    pass
+    MAX_OBJECTS = 20
+    polls = Poll.objects.all()[:MAX_OBJECTS]
+    data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
+    return JsonResponse(data)
 
 
 def poll_details(request, pk):
-    pass
+    poll = get_object_or_404(Poll, pk=pk)
+    data = {"results": {
+        "question": poll.question,
+        "created_by": poll.created_by.username,
+        "pub_date": poll.pub_date
+    }}
+
