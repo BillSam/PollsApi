1133c627ac5c08df36c0fb6bceda21a9c0a24cec
sam mbugua <sam@systech>
2022-02-01 20:56:22 +0300

    added users authentication

--- core/pollsApi/urls.py
+++ core/pollsApi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import poll_list, poll_details
-from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote, UserCreate
 from rest_framework.routers import DefaultRouter
 from .apiviews import PollViewSet
 
@@ -15,6 +15,7 @@ urlpatterns = [
 
     path("polls/<int:pk>/choices", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+    path("users/", UserCreate.as_view(), name="user_create"),
 
 ]
 
