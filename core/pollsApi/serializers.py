d9d0e123809a830ab6159517aa90cb0ffb1b950e
sam mbugua <sam@systech>
2022-02-02 14:00:40 +0300

    added tests tests

--- core/pollsApi/serializers.py
+++ core/pollsApi/serializers.py
@@ -12,16 +12,16 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            user = User(
-                email=validated_data['email'],
-                username=validated_data['username']
-            )
-            user.set_password(validated_data['password'])
-            user.save()
-            # user2 = User.objects.create_user(**validated_data)
-            Token.objects.create(user=user)
-            return user
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        # user2 = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 
 class VoteSerializer(serializers.ModelSerializer):
--- core/pollsApi/tests.py
+++ core/pollsApi/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APIRequestFactory
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+from .apiviews import PollViewSet
+'''
+APIRequestFactory: This is similar to Django’s RequestFactory:
+ It allows you to create requests with any http method, 
+ which you can then pass on to any view method and compare responses.
+APIClient: similar to Django’s Client. You can GET or POST a URL, and test responses.
+APITestCase: similar to Django’s TestCase. Most of your tests will subclass this. 
+'''
+
+
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.view = PollViewSet.as_view({'get': 'list'})
+        self.uri = '/polls/'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='testuser@gmail.com',
+            password='test'
+        )
+
+    def test_list(self):
+        request = self.factory.get(self.uri,
+                                   HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_create(self):
+        self.client.login(username='test', password='test')
+        params = {
+            "question": "How are you?",
+            "created_by": 1
+        }
+        response = self.client.post(self.uri, params)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'
+                         .format(response.status_code))
